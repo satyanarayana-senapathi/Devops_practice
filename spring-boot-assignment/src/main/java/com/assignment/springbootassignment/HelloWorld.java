7415a4cb0f6e1bd1ec7ba78d831b5b5a43e1deb3
satyanarayana-zemoso <satyanarayana.senapathi@zemosolabs.com>
2023-06-09 16:53:50 +0530

    add message

--- spring-boot-assignment/src/main/java/com/assignment/springbootassignment/HelloWorld.java
+++ spring-boot-assignment/src/main/java/com/assignment/springbootassignment/HelloWorld.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorld {
     @GetMapping("/")
     public String Welcome(){
-        return"hello world";
+        return"hello world,welcome to spring boot";
     }
 }
